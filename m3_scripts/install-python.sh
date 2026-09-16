b5ace852774200af9d1232c9672345845f56e982
Michal-Novomestsky <mishko.novo@gmail.com>
2026-09-16 13:35:20 +1000

    Consolidate requirements

--- m3_scripts/install-python.sh
+++ m3_scripts/install-python.sh
@@ -22,6 +22,5 @@ eval "$(./bin/micromamba shell hook -s bash -r "$MAMBA_ROOT")"
 micromamba activate "$ENV_PATH"
 
 pip3 install -r requirements.txt
-pip3 install "deepspeed<0.16" datasets tiktoken
 
 python --version
